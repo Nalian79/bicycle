8386a6ed3ad079f90157c49819a55f13a3539131
Michele <michele.morelock@gmail.com>
2014-11-09 21:13:33 +0000

    Fix the variable assingments so they're per-instance Fix the logic on the sell_bike method.

--- bike_classes.py
+++ bike_classes.py
@@ -8,61 +8,64 @@ class Bicycle(object):
 
 class BikeShop(object):
     """A BikeShop class for making and running your own bicycle shop."""
-    inventory = {}
-    bicycles = {}
-    profit = 0
     def __init__(self, name):
         self.name = name
+        self.inventory = {}
+        self.bicycles = {}
+        self.profit = 0
 
     def add_bike(self, bicycle, cost, number_of_bikes=1):
         """Add a bicycle to our inventory of bikes."""
-        if not bicycle in bicycles:
-            bicyles[bicycle] = cost
-            inventory[bicycle] = number_of_bikes
+        if not bicycle in self.bicycles:
+            self.bicycles[bicycle] = cost
+            self.inventory[bicycle] = number_of_bikes
             print bicycle + " added to inventory."
         else:
-            print bicycle + " is already part of inventory."
-        return bicycles, inventory
+            self.inventory[bicycle] = self.inventory[bicycle] + number_of_bikes 
+            print "You now have " + str(self.inventory[bicycle]) + \
+                " bicycles in inventory."
+        return self.bicycles, self.inventory
 
     def remove_bike(self, bicycle):
         """Just remove the bike from inventory."""
-        if bicycle in inventory:
-            if inventory[bicycle] > 1:
-                inventory[bicycle] = inventory[bicycle] - 1
+        if bicycle in self.inventory:
+            if self.inventory[bicycle] > 1:
+                self.inventory[bicycle] = self.inventory[bicycle] - 1
                 print "Removed 1 " + bicycle + " from inventory."
             else:
-                del bicycles[bicycle]
-                del inventory[bicycle]
+                del self.bicycles[bicycle]
+                del self.inventory[bicycle]
                 print "Removed the last " + bicycle + " from inventory."
         else:
             print bicycle + " is not in inventory."
-        return bicycles, inventory
+        return self.bicycles, self.inventory
 
-    def sell_bike(self, bicycle, margin):
+    def sell_bike(self, bicycle, margin=0.2):
         """Remove bike from inventory and add margin to the profits.
         Calculate the total cost from the bicycle price + margin, print that
         out, then add the margin to the profit, and delete the bike from
         inventory.
         """
-        if bicycle in inventory:
-            pricetag = inventory[bicycle] + (bicycles[bicycle] * int(margin))
+        if bicycle in self.inventory:
+            pricetag = int(self.bicycles[bicycle]) + (int(self.bicycles[bicycle]) * float(margin))
             print "Selling the bicycle " + bicycle + " for " + str(pricetag)
-            new_profit = pricetag - bicycles[bicycle]
-            profit = profit + new_profit
-            remove_bike(bicycle)
+            new_profit = pricetag - self.bicycles[bicycle]
+            self.profit = self.profit + new_profit
+            self.remove_bike(bicycle)
         else:
-            print "I do not have " + bicycle
-        return bicycles, inventory
+            print "I do not have any " + str(bicycle) + " in stock."
+        # remove the self.bicycles once all is working
+        return self.bicycles, self.inventory
         
 
 class Customer(object):
     """Create a customer object, with a source of funds and a
     dict of bikes owned.
     """
-    bicycles_owned = {}
     def __init__(self, name, fund=0.0):
         self.name = name
         self.fund = fund
+        self.bicycles_owned = {}
     def buy_bike(self, cost):
         "Remove the bike cost from customer fund, then return the fund amount."
         if cost > self.fund:
